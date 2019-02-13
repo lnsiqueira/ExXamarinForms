38df8d7cf1d8e910d6e9cb8339ffc46819a44f98
Luciano Siqueira <lnsiqueira@gmail.com>
2019-02-13 18:01:08 -0200

    Alteraçoes

---
+++ code/App01_consultarCep_Highcon/App01_consultarCep_Highcon/App01_consultarCep_Highcon/Servico/Modelo/ViaCEPServico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_consultarCep_Highcon.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_consultarCep_Highcon.Servico.Modelo
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscaEnderecoViaCEP(string cep)
+        {
+            string novoEnderecoRL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            //sincrono -- trava
+            string conteudo = wc.DownloadString(novoEnderecoRL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+
+
+        }
+    }
+}
